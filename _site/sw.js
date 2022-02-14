5347f1c7de6a5f33aca332bb489da2ca44ef1420
aohus <aohus@laivdata.com>
2022-02-15 00:04:10 +0900

    bundle update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220214.1301';
+const cacheName = 'chirpy-20220215.0003';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
